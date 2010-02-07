33baf868f7ddb42dc9d522bfe934f6f575da31f1
cdmckay <cdmckay@cdmckay.org>
2010-02-07 21:01:36 +0000

    Started working on the NBoolean class.  Added constructor.

--- System/NBoolean.php
+++ System/NBoolean.php
@@ -6,5 +6,16 @@ class NBoolean
     extends NObject
     implements IComparable, IFormattable, IConvertible, IEquatable
 {
+    private $value = false;
+
+    public function NBoolean($value = false)
+    {
+        $this->value = (bool) $value;
+    }
+
+    public function getHashCode()
+    {
+        return $this->value ? new NInteger(1) : new NInteger(0);
+    }
 
 }
